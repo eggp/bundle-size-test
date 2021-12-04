0ba89eab8f8221914ea8eafc8a4b72f48668849b
eggp <eggprojec@gmail.com>
2021-12-04 15:08:17 +0100

    initial commit

---
+++ apps/test1/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bundle-size-test-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test1';
+}
