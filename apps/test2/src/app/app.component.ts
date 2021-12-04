1daa75e0b1d9205b1000401d92ded3a5aaa2acbc
eggp <eggprojec@gmail.com>
2021-12-04 15:32:39 +0100

    add test code

--- apps/test2/src/app/app.component.ts
+++ apps/test2/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { environment } from '../environments/environment';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'bundle-size-test-root',
@@ -7,7 +7,6 @@ import { environment } from '../environments/environment';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
   constructor() {
     if (environment.production === false) {
       console.log('uzike1');
---
+++ apps/test2/src/environments/environment.prod.ts
@@ -0,0 +1,3 @@
+export const environment:Readonly<{ production:boolean }> = {
+  production: true,
+};
